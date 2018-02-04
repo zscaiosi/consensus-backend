0d39ec2f2d5d5ae9de1af592c10163efb83b76fa
zscaiosi <zscaio.si@gmail.com>
2018-02-04 14:48:13 -0200

    [Enter, Leave, Create] Fluxo total de criar, entrar e sair de salas;

--- src/controllers/SocketController.ts
+++ src/controllers/SocketController.ts
@@ -1,8 +1,11 @@
 const moment = require('moment');
 const mongodbClient = require('mongodb').MongoClient;
+// const {mongodbUrl} = require('../constants.json');
+const Participant = require('../models/Participant');
 
 interface Room {
   roomName: string,
+  clientId: string,
   clientName: string
 }
 
@@ -14,35 +17,46 @@ function ServerSocketController(ioServer?: any){
 ServerSocketController.prototype.listenToConnection = function(){
   //Espera por uma conexão
   this._socket.on('connection', (clientSocket) => {
-
+    // Quando recebe uma mensagem para entrada em sala
     clientSocket.on('enter_room', (room: Room) => {
-      console.log(room)
+      console.log("Enter: ", room);
 
-      mongodbClient.connect('mongodb://localhost:27017/usuarios', (err, db) => {
+      const p = new Participant(room.clientId, room.clientName, room.roomName);
+      p.registerParticipation(room.clientId, room.clientName, room.roomName, (status, result) => {
 
-        if (!err) {
-    
-          const dbConn = db.db('usuarios');
-    
-          dbConn.collection('rooms').findOne({ name: room.roomName }, (findErr, result) => {
-    
-            if ( !findErr ) {
-              //Adiciona à sala desejada
-              clientSocket.join(room.roomName);
-
-              this._socket.in(room.roomName).emit('new_client', `${room.clientName} acabou de entrar na sala!`);
-            } else {
-              this._socket.emit(room.clientName, 'A sala solicitada não existe!');
-            }
-    
-          });
-    
+        if (status === 200) {
+          //Adiciona à sala desejada
+          clientSocket.join(room.roomName);
+          //Notifica os participantes da sala sobre a entrada do novo usuário
+          this._socket.in(room.roomName).emit('new_client', `${room.clientName} acabou de entrar na sala!`);
         } else {
-          console.log(err);
+          this._socket.emit(room.clientId, 'A sala solicitada não existe!');
         }
-    
+
       });
+      
     });
+    
+    // Quando recebe uma mensagem para saída de sala
+    clientSocket.on('exit_room', (room: Room) => {
+      console.log("Exit: ", room);
+
+      const p = new Participant(room.clientId, room.clientName, room.roomName);
+      p.registerExit(room.clientId, room.clientName, room.roomName, (status, result) => {
+
+        if (status === 200) {
+          // Abandona a sala
+          this._socket.leave(room.roomName);
+          // Notifica os participantes da sala sobre a saída do usuário
+          this._socket.in(room.roomName).emit('client_left', `${room.clientName} acabou de sair da sala!`);
+        } else {
+          this._socket.emit(room.clientId, `${room.clientName}, não foi possível registrar sua saída, por favor, tente novamente! \n Error: ${result}`);
+        }
+
+      });
+
+    });
+
 
   });
 
@@ -50,21 +64,21 @@ ServerSocketController.prototype.listenToConnection = function(){
 
 ServerSocketController.prototype.createRoom = function(name: string, next: Function){
 
-  mongodbClient.connect('mongodb://localhost:27017/usuarios', (err, db) => {
+  mongodbClient.connect(`mongodb://localhost:27017/consensus_db`, (err, db) => {
 
     if (!err) {
 
-      const dbConn = db.db('usuarios');
+      const dbConn = db.db('consensus_db');
       const generatedId = moment(new Date()).format()+"_"+name;
 
-      dbConn.collection('rooms').insert({ _id: generatedId, name: name }, (insertErr, result) => {
+      dbConn.collection('rooms').insert({ _id: generatedId, roomName: name, open: true }, (insertErr, result) => {
 
         if ( !insertErr ) {
           next(200, result);
         } else {
           next(500, insertErr);
         }
-
+        db.close();
       });
 
     } else {
@@ -75,7 +89,28 @@ ServerSocketController.prototype.createRoom = function(name: string, next: Funct
 
 }
 
-ServerSocketController.prototype.enterRoom = function(name: string){
+ServerSocketController.prototype.enterRoom = function(name: string, next: Function){
+
+  mongodbClient.connect(`mongodb://localhost:27017/consensus_db`, (err, db) => {
+
+    if ( !err ) {
+
+      const dbConn = db.db('consensus_db');
+      
+      dbConn.collection('rooms').findOne({ roomName: name, open: true }, (findErr, result) => {
+
+        if ( !findErr ) {
+          next(200, result);
+        } else {
+          next(500, findErr);
+        }
+        db.close();
+      });
+    } else {
+      console.log(err);
+    }
+
+  });
 
 }
 
